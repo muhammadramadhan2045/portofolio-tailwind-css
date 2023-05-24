2f8d6948a5e7c6b59d9a7fea73fc1349bbfa6897
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-05-25 06:05:16 +0700

    portofolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+//navbar fixed
+window.onscroll=function(){
+    const heeader = document.querySelector('header');
+    const fixedNav=heeader.offsetTop;
+    if(window.pageYOffset > fixedNav){
+        heeader.classList.add('navbar-fixed');
+    }else{
+        heeader.classList.remove('navbar-fixed');
+    }
+}
+
+//ini untuk bagian hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
